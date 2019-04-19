08abfaac879f81181a4afeb13f63662e8daaaa1f
xnorax <noora431@gmail.com>
2019-04-19 16:25:08 +0300

    add units

---
+++ unit3/1-functions-print3.py
@@ -0,0 +1,9 @@
+# print3 function definition
+def print3(word):
+    print(word,"*")
+    print(word,"*")
+    print(word,"*")
+    
+    
+# call print3 function   
+print3("test")
---
+++ unit3/2-functions-convert.py
@@ -0,0 +1,8 @@
+# convert function definition
+def convert(fahrenheit):
+    celsius = (fahrenheit - 32) * 5.0/9.0
+    print(celsius)
+    
+    
+# call convert function   
+convert(100)
