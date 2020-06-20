5d9eb1f5b7d8740759b15a8e304ad91bfd8b8290
Asmaa AlSaggaf <aumsaggaf@users.noreply.github.com>
2020-06-20 18:57:36 +0300

    added content
    added content for day 2

---
+++ teens-program/day2/challenge1.py
@@ -0,0 +1,8 @@
+# Try to print the following with some modefications:
+# "Hello world, my name is <<name>> and I am learning Python. I am from <<city>> and I am <<age>> years old."
+
+name = 'Asmaa'
+city = 'Jeddah'
+age = 25
+
+print('Hello world, my name is ' + name + ' and I am learning Python. I am from ' + city +' and I am ' + str(25) + ' years old.')
---
+++ teens-program/day2/demo2.py
@@ -0,0 +1,4 @@
+name = 'Asmaa'
+num_of_sisters = 2
+
+print('My name is ' + name + ' and I have '+ str(num_of_sisters) + ' sisters.')
